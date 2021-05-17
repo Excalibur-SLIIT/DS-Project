dfecc1a1f0b0949f9ee6ce1d9d32ee029871128b
Maduka Nuwantha <mbnuwantha90@gmail.com>
2021-05-17 19:47:14 +0530

    Cart front end done

--- src/Pages/Cart/Cart.js
+++ src/Pages/Cart/Cart.js
@@ -3,15 +3,21 @@ import Footer from '../../Components/Common/Footer/Footer'
 import Header from '../../Components/Common/Header/Header'
 import BillingModule from '../../Modules/CartModules/BillingModule/BillingModule'
 import CartProductsModule from '../../Modules/CartModules/CartProductsModule/CartProductsModule'
+import HeadingModule from '../../Modules/CartModules/HeadingModule/HeadingModule'
 
 class Cart extends Component {
     render() {
         return (
             <div>
-                <Header/>
-                <CartProductsModule/>
-                <BillingModule/>
-                <Footer/>
+                <Header />
+                <HeadingModule />
+                <div class="container">
+                    <div class="row">
+                        <CartProductsModule />
+                        <BillingModule />
+                    </div>
+                </div>
+                <Footer />
             </div>
         )
     }
--- src/Routes/RoutesComponent.js
+++ src/Routes/RoutesComponent.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import CartProductsModule from '../Modules/CartModules/CartProductsModule/CartProductsModule';
+import Cart from '../Pages/Cart/Cart';
 import Home from '../Pages/Home/Home';
 import SignIn from '../Pages/SignIn/SignIn';
 
@@ -10,6 +12,7 @@ class RoutesComponent extends Component {
                 <Switch>
                     <Route path="/" exact component={Home}/>
                     <Route path="/signin" exact component={SignIn}/>
+                    <Route path="/cart" exact component={Cart}/>
                 </Switch>
             </Router>
         )
