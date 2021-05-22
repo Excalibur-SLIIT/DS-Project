b793e6ffb842caca4c6a8ef3bbb90a91053dc2ca
DilushaC <dilushafernando97@gmail.com>
2021-05-23 01:21:17 +0530

    Seller profile edit attempt fail

--- src/Modules/SellerProfileModules/SellerDetails/SellerDetails.js
+++ src/Modules/SellerProfileModules/SellerDetails/SellerDetails.js
@@ -1,78 +1,92 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class SellerDetails extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: "",
+            fname: "",
+            lname: "",
+            companyName: "",
+            email: "",
+            mobile: "",
+            address: "",
+            password: ""
+        }
+    }
+
+    componentDidMount() {
+        const config = {
+            headers: {
+                "x-auth-token": localStorage.getItem("x-auth-token")
+            }
+        }
+
+        axios.post("http://localhost:5000/seller/auth", "", config).then((res) => {
+            this.setState({
+                username: res.data.username,
+                fname: res.data.fname,
+                lname: res.data.lname,
+                password: res.data.password,
+                companyName: res.data.companyName,
+                email: res.data.email,
+                mobile: res.data.mobile,
+                address: res.data.address,
+            })
+        }).catch((err) => { })
+
+    }
+
     render() {
         return (
             <div>
-
                 <hr class="offset-lg"></hr>
-
                 <div class="product">
                     <div class="container">
                         <div class="row">
-
                             <br /><br /><br />
-
-
                             <div class="row">
-
-
                                 <div style={{ width: '50%', margin: 'auto' }}>
                                     <div class="col-sm-7" style={{ width: '100.3333333%' }}>
-                                        <h1 style={{ textAlign: 'center' }}>user name</h1>
+                                        <h1 style={{ textAlign: 'center' }}>{this.state.username}</h1>
                                         <br />
                                         <hr />
                                         <br />
 
                                         <h3>Profile Details</h3>
                                         <br />
-
                                         <div class="row specification">
-                                            <div class="col-sm-6">
-                                                <label>Operating system</label>
-                                            </div>
-                                            <div class="col-sm-6"> <p>Windows</p> </div>
+                                            <div class="col-sm-6"><label>First Name</label></div>
+                                            <div class="col-sm-6"> <p>{this.state.fname}</p> </div>
                                         </div>
 
                                         <div class="row specification">
-                                            <div class="col-sm-6"> <label>Processor</label> </div>
-                                            <div class="col-sm-6"> <p>Intel® Celeron® N2840 with Intel® HD Graphics (2.16 GHz, up to 2.58 GHz, 1 MB cache, 2 cores)</p> </div>
+                                            <div class="col-sm-6"> <label>Last Name</label> </div>
+                                            <div class="col-sm-6"> <p>{this.state.lname}</p> </div>
                                         </div>
 
                                         <div class="row specification">
-                                            <div class="col-sm-6"> <label>Processor technology</label> </div>
-                                            <div class="col-sm-6"> <p>Intel Turbo Boost Technology</p> </div>
+                                            <div class="col-sm-6"> <label>Company</label> </div>
+                                            <div class="col-sm-6"> <p>{this.state.companyName}</p> </div>
                                         </div>
 
                                         <div class="row specification">
-                                            <div class="col-sm-6"> <label>Graphics</label> </div>
-                                            <div class="col-sm-6"> <p>Intel HD Graphics</p> </div>
+                                            <div class="col-sm-6"> <label>Email</label> </div>
+                                            <div class="col-sm-6"> <p>{this.state.email}</p> </div>
                                         </div>
 
                                         <div class="row specification">
-                                            <div class="col-sm-6"> <label>Memory</label> </div>
-                                            <div class="col-sm-6"> <p>2 GB DDR3L SDRAM (onboard)</p> </div>
+                                            <div class="col-sm-6"> <label>Mobile</label> </div>
+                                            <div class="col-sm-6"> <p>{this.state.mobile}</p> </div>
                                         </div>
 
                                         <div class="row specification">
-                                            <div class="col-sm-6"> <label>Hard drive</label> </div>
-                                            <div class="col-sm-6"> <p>16 GB eMMC</p> </div>
+                                            <div class="col-sm-6"> <label>Address</label> </div>
+                                            <div class="col-sm-6"> <p>{this.state.address}</p> </div>
                                         </div>
 
-                                        <div class="row specification">
-                                            <div class="col-sm-6"> <label>Wireless</label> </div>
-                                            <div class="col-sm-6"> <p>802.11ac (2x2) and Bluetooth® 4.0 combo</p> </div>
-                                        </div>
-
-                                        <div class="row specification">
-                                            <div class="col-sm-6"> <label>Power supply</label> </div>
-                                            <div class="col-sm-6"> <p>45 W AC power adapter</p> </div>
-                                        </div>
-
-                                        <div class="row specification">
-                                            <div class="col-sm-6"> <label>Battery</label> </div>
-                                            <div class="col-sm-6"> <p>3-cell, 36 Wh Li-ion</p> </div>
-                                        </div>
                                     </div>
                                 </div>
 
--- src/Modules/SellerProfileModules/SellerEditProfile/FormModule.js
+++ src/Modules/SellerProfileModules/SellerEditProfile/FormModule.js
@@ -1,6 +1,124 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class FormModule extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangeFname = this.onChangeFname.bind(this);
+        this.onChangeLname = this.onChangeLname.bind(this);
+        this.onChangeCompany = this.onChangeCompany.bind(this);
+        this.onChangeEmail = this.onChangeEmail.bind(this);
+        this.onChangeMobile = this.onChangeMobile.bind(this);
+        this.onChangeAddress = this.onChangeAddress.bind(this);
+        this.onChangePassword = this.onChangePassword.bind(this);
+
+        this.state = {
+            username: "",
+            fname: "",
+            lname: "",
+            companyName: "",
+            email: "",
+            mobile: "",
+            address: "",
+            password: ""
+        }
+    }
+
+    componentDidMount() {
+        const config = {
+            headers: {
+                "x-auth-token": localStorage.getItem("x-auth-token")
+            }
+        }
+
+        axios.post("http://localhost:5000/seller/auth", "", config).then((res) => {
+            this.setState({
+
+                username: res.data.username,
+                fname: res.data.fname,
+                lname: res.data.lname,
+                companyName: res.data.companyName,
+                email: res.data.email,
+                mobile: res.data.mobile,
+                address: res.data.address,
+                password: res.data.password,
+
+            })
+        }).catch((err) => { })
+
+    }
+
+    onChangeUsername(event) {
+        this.setState({
+            username: event.target.value
+        });
+    }
+    onChangeFname(event) {
+        this.setState({
+            fname: event.target.value
+        });
+    }
+    onChangeLname(event) {
+        this.setState({
+            lname: event.target.value
+        });
+    }
+    onChangeCompany(event) {
+        this.setState({
+            companyName: event.target.value
+        });
+    }
+    onChangeEmail(event) {
+        this.setState({
+            email: event.target.value
+        });
+    }
+    onChangeMobile(event) {
+        this.setState({
+            mobile: event.target.value
+        });
+    }
+    onChangeAddress(event) {
+        this.setState({
+            address: event.target.value
+        });
+    }
+    onChangePassword(event) {
+        this.setState({
+            password: event.target.value
+        });
+    }
+
+    onSubmit(e) {
+
+        e.preventDefault();
+
+        const Seller = {
+
+            username: this.state.username,
+            fname: this.state.fname,
+            lname: this.state.lname,
+            companyName: this.state.companyName,
+            email: this.state.email,
+            mobile: this.state.mobile,
+            address: this.state.address,
+            password: this.state.password,
+
+        }
+
+        console.log(Seller);
+
+        axios.post("http://localhost:5000/seller/auth", "", Seller)
+            .then(res => console.log(res.data));
+
+        window.location = '../sellerprofile';
+    }
+
+
+
     render() {
         return (
             <div>
@@ -11,32 +129,88 @@ class FormModule extends Component {
                         <div class="col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4 md-padding">
                             <h1 class="align-center">Seller Edit Pofile</h1>
                             <br />
-                            <form class="join" action="./sellerprofile">
+                            <form class="join" onSubmit={this.onSubmit}>
                                 <div class="container-fluid">
                                     <div class="row">
                                         <div class="col-sm-12">
-                                            <input type="text" name="username" placeholder="Username" required="" class="form-control" /><br />
+                                            <input type="text"
+                                                name="username"
+                                                placeholder="Username"
+                                                required=""
+                                                class="form-control"
+                                                value={this.state.username}
+                                                onChange={this.onChangeUsername}
+                                            /><br />
                                         </div>
                                         <div class="col-sm-12">
-                                            <input type="text" name="fname" placeholder="First Name" required="" class="form-control" /><br />
+                                            <input type="text"
+                                                name="fname"
+                                                placeholder="First Name"
+                                                required=""
+                                                class="form-control"
+                                                value={this.state.fname}
+                                                onChange={this.onChangeFname}
+                                            /><br />
                                         </div>
                                         <div class="col-sm-12">
-                                            <input type="text" name="lname" placeholder="Last Name" required="" class="form-control" /><br />
+                                            <input type="text"
+                                                name="lname"
+                                                placeholder="Last Name"
+                                                required=""
+                                                class="form-control"
+                                                value={this.state.lname}
+                                                onChange={this.onChangeLname}
+                                            /><br />
                                         </div>
                                         <div class="col-sm-12">
-                                            <input type="text" name="company" placeholder="Company" required="" class="form-control" /><br />
+                                            <input type="text"
+                                                name="company"
+                                                placeholder="Company"
+                                                required=""
+                                                class="form-control"
+                                                value={this.state.companyName}
+                                                onChange={this.onChangeCompany}
+                                            /><br />
                                         </div>
                                         <div class="col-sm-12">
-                                            <input type="text" name="mobile" placeholder="Phone" required="" class="form-control" /><br />
+                                            <input type="text"
+                                                name="mobile"
+                                                placeholder="Phone"
+                                                required=""
+                                                class="form-control"
+                                                value={this.state.mobile}
+                                                onChange={this.onChangeMobile}
+                                            /><br />
                                         </div>
                                         <div class="col-sm-12">
-                                            <input type="email" name="email" placeholder="E-mail" required="" class="form-control" /><br />
+                                            <input type="email"
+                                                name="email"
+                                                placeholder="E-mail"
+                                                required=""
+                                                class="form-control"
+                                                value={this.state.email}
+                                                onChange={this.onChangeEmail}
+                                            /><br />
                                         </div>
                                         <div class="col-sm-12">
-                                            <input type="text" name="address" placeholder="Address" required="" class="form-control" /><br />
+                                            <input type="text"
+                                                name="address"
+                                                placeholder="Address"
+                                                required=""
+                                                class="form-control"
+                                                value={this.state.address}
+                                                onChange={this.onChangeAddress}
+                                            /><br />
                                         </div>
                                         <div class="col-sm-12">
-                                            <input type="password" name="password" placeholder="Password" required="" class="form-control" /><br />
+                                            <input type="password"
+                                                name="password"
+                                                placeholder="Password"
+                                                required=""
+                                                class="form-control"
+                                                value={this.state.password}
+                                                onChange={this.onChangePassword}
+                                            /><br />
                                         </div>
                                     </div>
                                 </div>
